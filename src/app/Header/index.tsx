5a076ee35ea18eb3783c4f26d3ad45d1ea231423
RedaHamouche <reda.hamouche@outlook.com>
2025-06-21 13:48:33 +0200

    Header, fonts, design system wip

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,12 +1,54 @@
-"use state"
+"use client"
+import {useEffect, useState} from 'react';
 import styles from './index.module.scss';
+import classnames from 'classnames';
 
+type announcementsType = {
+  color: string
+  text: string
+}
 
-function Header() {
+export type HeaderType = {
+  announcements?: announcementsType[]
+}
+
+function Header({announcements = []}: HeaderType) {
+  // useEffect(() => {
+  //   const rootStyle = document.documentElement?.style;
+  //   rootStyle.setProperty(
+  //     '--header-width',
+  //     `${headerSize}px`
+  //   );
+  // }, [headerOpen, headerSize]);
 
   return (
-    <header className={styles.header}>
-   
+    <header className={styles.header}>   
+      <div className={styles.announce}>
+        <div className={styles.scrollContainer}>
+          {announcements.map((announce) => {
+            const {color, text} = announce;
+            return (
+                <div key={text} className={styles.freelance}>
+                  <span className={classnames(styles.dot, styles[color])} />
+                    <p className={styles.text}>
+                      {text}
+                    </p>
+                </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <nav>
+        <a href="#">Hamouche Reda</a>
+        <div>Banniere ici</div>
+        <ul>
+          <li>projects</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav> */}
+
     </header>
   );
 }

b0b36a8f1717cf7e34c9ba501e778bcd314eacf9
RedaHamouche <reda.hamouche@outlook.com>
2025-07-02 18:04:23 +0200

    Changed imports folders Map

--- src/app/MapScroller/components/SvgPath/index.tsx
+++ src/app/MapScroller/components/SvgPath/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../index.module.scss';
+import styles from './index.module.scss';
 import { MAP_PADDING_RATIO } from '@/config/mapPadding';
 
 interface SvgPathProps {
